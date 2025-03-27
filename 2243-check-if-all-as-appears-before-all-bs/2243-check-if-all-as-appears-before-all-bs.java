a3123ab4d16a1e7c70a3b11f48057718ccbd5448
Betsinat <Betsinat39@gmail.com>
2025-03-27 21:56:48 +0100

    Time: 0 ms (100%), Space: 41.9 MB (21.8%) - LeetHub

---
+++ 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkString(String s) {
+         boolean foundB = false;
+        for (char c : s.toCharArray()) {
+            if (c == 'b') {
+                foundB = true; 
+            } else if (foundB) {
+                return false; 
+            }
+        }
+        return true;
+        
+    }
+}
