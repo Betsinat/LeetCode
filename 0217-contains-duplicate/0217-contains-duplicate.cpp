5cb3173f377efbe29fac89593bab347256a8b6d5
Betsinat <Betsinat39@gmail.com>
2025-04-17 21:16:04 +0300

    Time: 23 ms (82.52%), Space: 70.9 MB (73.68%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int left = 0;
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        while(left < n-1){
-            if (nums[left]==nums[left+1]){
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; i++) {
+            
+            if (nums[i] == nums[i + 1]) {
                 return true;
             }
-            else{
-                left++;
-            }
         }
-                return false;
+        return false;
     }
 };
