ee1dc58eecf9a24ef2a969cac435587a2c30a7d7
Betsinat <Betsinat39@gmail.com>
2025-07-23 23:53:47 +0300

    Time: 11 ms (31.02%), Space: 12.6 MB (65.24%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
@@ -1,8 +1,6 @@
 class Solution(object):
     def divideArray(self, nums):
         frequency = Counter(nums)
-        if len(nums) % 2 == 1:
-            return False
         for count in frequency.values():
             if count % 2 == 1:
                 return False
