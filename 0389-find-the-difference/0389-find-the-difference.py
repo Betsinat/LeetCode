b6bc5a56f264c21fb9c7978423d6f683e0981049
Betsinat <Betsinat39@gmail.com>
2025-07-28 17:06:16 +0300

    Time: 7 ms (33.72%), Space: 12.4 MB (90.63%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        a = sorted(s)
+        b = sorted (t)
+        for i in range(len(s)):
+            if a[i] != b[i]:
+                return b[i]
+        return b[-1]
+
+                
+
+
+       
+      
+
+     
+        
