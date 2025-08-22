120a0d103d6baf908e17d40a85dfe6d8d38a5d1c
Betsinat <Betsinat39@gmail.com>
2025-08-22 22:48:51 +0300

    Time: 4 ms (28%), Space: 17.9 MB (36.29%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -5,11 +5,8 @@ class Solution:
                 return -1
             else:
                 return 1
-    
-        # nums = list(map(str, nums))
         nums = [str(i) for i in nums]
         nums.sort(key=cmp_to_key(compare))
-
         print(nums)
         result = "".join(nums)
         return result if result[0] != "0" else "0"
