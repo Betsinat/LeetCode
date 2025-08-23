671609d41225901582b334440b3558ead4b4b5b4
Betsinat <Betsinat39@gmail.com>
2025-08-24 02:41:03 +0300

    Time: 0 ms (100%), Space: 13.6 MB (71.16%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        min_price = prices[0]
+        '''min_price = prices[0]
         max_profit = 0
         for i in range(1 , len(prices)):
             if prices[i] > min_price:
@@ -12,7 +12,12 @@ class Solution(object):
                 min_price = prices[i]
             else:
                 min_price = prices[i]
-
+        return max_profit '''
+        max_profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                max_profit += prices[i] - prices[i-1]
         return max_profit
+
 
         
