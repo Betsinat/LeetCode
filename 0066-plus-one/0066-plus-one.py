0f78e254c6ab46d5108f78df47b48dc22d732fc0
Betsinat <Betsinat39@gmail.com>
2025-08-31 14:52:07 +0300

    Time: 0 ms (100%), Space: 12.5 MB (9.75%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        dig = ""
+        for i in range(len(digits)):
+            dig += str(digits[i])
+
+
+        b = int(dig)
+        b = b + 1
+        c = str(b)
+        for i in range(len(c)):
+            res.append(int(c[i]))
+        return res
+
+            
+
+        
