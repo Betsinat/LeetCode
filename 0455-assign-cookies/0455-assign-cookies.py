68bcc37a162710f639e2a689752a3d3c17f2259f
Betsinat Wendwesen <Betsinat39@gmail.com>
2025-12-02 02:26:49 +0300

    Time: 22 ms (56.03%), Space: 19.8 MB (63.22%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        cookie = 0
+        child = 0
+        while cookie < len(s) and child < len(g):
+            if s[cookie] >= g[child]:
+                child += 1
+            cookie +=1
+        return child
+
+           
