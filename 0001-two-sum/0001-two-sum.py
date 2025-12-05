8bbb9b2d535c8e661c9a1ccd9e5af4572580f237
Betsinat Wendwesen <Betsinat39@gmail.com>
2025-12-05 22:55:48 +0300

    Time: 1 ms (62.77%), Space: 13.5 MB (15.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,18 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        left, right = 0, len(nums) - 1
+        # storing the individual element of the array with its index
+        orginal_index = [(num, i) for i, num in enumerate(nums)]
+        orginal_index.sort()
         arr = []
-        for i in range(len(nums)):
-            for j in range(i+1 ,len(nums)):
-                if nums[i] + nums[j] ==target:
-                    arr.append(i)
-                    arr.append(j)
-        return arr
-
-    
-                      
+        while left < right:
+            if orginal_index[left][0] + orginal_index[right][0] == target:
+                arr.append(orginal_index[left][1])
+                arr.append(orginal_index[right][1])
+                break
+            elif orginal_index[left][0] + orginal_index[right][0] < target:
+                left += 1
+            else:
+                right -= 1
+        
+        return arr  # This must be indented inside the function
