755e2340236a6dc5bf1333259bbb5a43863c0c46
Betsinat Wendwesen <Betsinat39@gmail.com>
2025-12-06 00:30:33 +0300

    Time: 31 ms (75.23%), Space: 14 MB (96.49%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -1,13 +1,29 @@
-class Solution:
-    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        # scanning the whole children gridness , and scanning the size cookie
+        #if the cookie can satsfy the child and not taken give him o(n*m)
+
         g.sort()
         s.sort()
-        cookie = 0
-        child = 0
-        while cookie < len(s) and child < len(g):
-            if s[cookie] >= g[child]:
-                child += 1
-            cookie +=1
-        return child
+        #childern
+        i = 0
+        #cookie 
+        j = 0
+        satsfied_childrens = 0
+        while i < len(g) and j < len(s):
+           if s[j] >= g [i]:
+              satsfied_childrens +=1
+              i +=1
+              j +=1
+           else:
+              j +=1
+        return satsfied_childrens
 
-           
+    
+
+        
