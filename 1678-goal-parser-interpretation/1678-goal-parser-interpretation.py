d717ff8fedf4ac9ce132e3558c044a7b0967af26
Betsinat Wendwesen <Betsinat39@gmail.com>
2025-12-06 21:54:53 +0300

    Time: 24 ms (5.11%), Space: 12.5 MB (10.23%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,17 +1,19 @@
 class Solution(object):
     def interpret(self, command): 
         interpreted =""
-        for i in range(len(command)):
+        i = 0
+        n = len(command)
+        while i < n:
             if command[i] == "G":
-                interpreted +=(command[i])
-            elif command[i]=="(" and command[i+1] == ")":
-                interpreted +=("o")
-                i += 2
-            elif command[i] =="(" and command[i+1] == "a":
-                interpreted +=("al")
-                i += 4
+                interpreted += "G"
+            elif command[i:i+2] == "()":
+                interpreted += "o"
+            elif command[i:i+4] =="(al)":
+                interpreted += "al"
+            i+=1
         return interpreted
-
+                
+        
         
         
 
