d360abe98d359cac6925a65a009de5852130085f
Betsinat Wendwesen <Betsinat39@gmail.com>
2025-12-06 22:04:26 +0300

    Time: 4 ms (55.3%), Space: 13.5 MB (13.88%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,8 +1,15 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-       arr = [i for i in range(len(nums) + 1)]
-       for i in range(len(arr)):
-          if arr[i] not in nums:
-            return arr[i]
-        
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        arr = []
+        n = len(nums)
+        for i in range(n+1):
+            arr.append(i)
+        return sum(arr) - sum(nums)
+       
+
+
         
