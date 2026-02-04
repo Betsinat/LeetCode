10cf7ccbf73d56c61a6432df4c1caf97278ab065
Betsinat Wendwesen <Betsinat39@gmail.com>
2026-02-04 14:51:02 +0300

    Time: 7 ms (63.55%), Space: 19.3 MB (28.83%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,4 +1,16 @@
-class Solution(object):
-    def isPalindrome(self, x):
-     return str(x) == str(x)[::-1]
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        left = 0
+        right = (len(x)) - 1
+        for _ in range(len(x)):
+            if x[left] == x[right]:
+                left += 1
+                right -= 1
+            else:
+                return False
+        return True
+
+            
+            
         
